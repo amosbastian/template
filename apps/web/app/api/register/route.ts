33afab9896650fe7a4afbd8499d06becdf923fd6
Amos Bastian <amosbastian@gmail.com>
2023-05-28 21:13:10 +0200

    feat: add generateToken function

--- apps/web/app/api/register/route.ts
+++
@@ -1,6 +0,0 @@
-import { signUp } from "@template/feature/authentication/server";
-import { NextRequest } from "next/server";
-
-export async function POST(request: NextRequest) {
-  return signUp(request);
-}
--- libs/utility/shared/src/index.ts
+++ libs/utility/shared/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./lib/classnames/classnames";
+export * from "./lib/generate-token/generate-token";
 export * from "./lib/get-first-part-of-email/get-first-part-of-email";
